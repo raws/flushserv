d669611378aa36e469e65f77fa14aa2f0452fed8
Ross Paffett <ross@rosspaffett.com>
2010-06-17 13:15:11 -0400

    Initial commit

---
+++ flushserv.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require "open3"
+require "rubygems"
+require "sinatra"
+
+get "/" do
+  if params[:message]
+    
+  end
+end
+
+helpers do
+  def toilet(message)
+    
+  end
+  
+  def extract_options!
+    
+  end
+end
